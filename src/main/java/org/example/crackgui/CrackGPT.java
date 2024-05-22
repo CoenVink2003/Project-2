3a0aa14b47ab1386266a18dbbb77590ba7005690
zZProgenitorZz <shantosh-2002@hotmail.nl>
2024-05-22 16:54:41 +0200

    menu 1

--- src/main/java/org/example/crackgui/CrackGPT.java
+++ src/main/java/org/example/crackgui/CrackGPT.java
@@ -16,11 +16,15 @@ public class CrackGPT extends Application {
     private SettingsComponent settings;
     private PromptComponent prompt;
 
+    MenuComponent menu = new MenuComponent(this);
+    BorderPane root = new BorderPane();
+
     public ResourceBundle language;
 
     public void start(Stage stage) throws IOException {
-        this.language =  ResourceBundle.getBundle("org.example.crackgui.messages", new Locale("nl"));;
-        Scene scene = prompScreen(800, 600);
+        this.language =  ResourceBundle.getBundle("org.example.crackgui.messages", new Locale("nl"));
+        menu.menuInitialize();
+        Scene scene = createScene();
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         stage.setTitle("CrackGPT-4");
@@ -30,72 +34,35 @@ public class CrackGPT extends Application {
         stage.show();
     }
 
-    private Scene prompScreen(int width, int height) {
-        // Background container
-        VBox body = new VBox();
-        body.setStyle("-fx-background-color: #2067DC;");
-        body.getStyleClass().add("body");
+    public Scene createScene() {
+        root = new BorderPane();
+        ChatScene body = new ChatScene(this);
 
-        // Header label
-        Label title = new Label("CrackGPT-4");
-        title.getStyleClass().add("crackgpt-label");
-        title.setMaxWidth(Double.MAX_VALUE);
-        title.setAlignment(Pos.CENTER);
+        root.setCenter(body.createBody());
+        root.setTop(menuButton());
 
-        // Input area
-        TextArea inputArea = new TextArea();
-        inputArea.setWrapText(true);
-        StackPane inputAreaPane = new StackPane();
-        inputAreaPane.setPadding(new Insets(0, 15, 0, 15));
-        inputAreaPane.getChildren().add(inputArea);
+        Scene scene = new Scene(root,800, 600);
+        return scene;
+    }
 
-        // Settings screen
-        settings = new SettingsComponent(this);
-        GridPane settingsPane = settings.generate();
-
-        // AI output area
-        TextArea outputArea = new TextArea();
-        outputArea.setWrapText(true);
-        outputArea.setEditable(false);
-        StackPane textAreaOutputPane = new StackPane();
-        textAreaOutputPane.setPadding(new Insets(20, 15, 20, 15));
-        textAreaOutputPane.getChildren().add(outputArea);
-
-        // Prompt button
-        prompt = new PromptComponent(this);
-        Button talkButton = prompt.generate(inputArea, settings.getLanguage(), outputArea);
-        StackPane talkButtonPane = new StackPane();
-        talkButtonPane.setPadding(new Insets(40, 20, 0, 20));
-        talkButtonPane.getChildren().add(talkButton);
-
+    public HBox menuButton(){
         // Root layout
-        BorderPane root = new BorderPane();
         HBox header = new HBox();
         header.setAlignment(Pos.CENTER_LEFT);
 
         // Menu
-        MenuComponent menu = new MenuComponent(this);
         Button hamburgerButton = new Button("\u2630");
         hamburgerButton.getStyleClass().add("hamburger-button");
-         hamburgerButton.setOnAction(event -> {
-             if (root.getLeft() == null) {
-                 root.setLeft(menu.generate());
-             } else {
-                 root.setLeft(null);
-             }
-         });
+        hamburgerButton.setOnAction(event -> {
+            if (root.getLeft() == null) {
+                root.setLeft(menu.getRoot());
+            } else {
+                root.setLeft(null);
+            }
+        });
         header.getChildren().add(hamburgerButton);
 
-        // Add elements to the body
-        body.getChildren().addAll(title, inputAreaPane, settingsPane, talkButtonPane, textAreaOutputPane);
-
-        // Set elements in root layout
-        root.setTop(header);
-        root.setCenter(body);
-
-        // Create and return the scene
-        Scene scene = new Scene(root, width, height);
-        return scene;
+        return header;
     }
 
     public SettingsComponent getSettings()
--- src/main/java/org/example/crackgui/MenuComponent.java
+++ src/main/java/org/example/crackgui/MenuComponent.java
@@ -2,27 +2,122 @@ package org.example.crackgui;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class MenuComponent {
+
+    TabPane chatTabPane;
+    int chatCounter = 1;
+    private ListView<String> chatHistoryListView;
+
     private CrackGPT application;
+
+    BorderPane root;
+    public BorderPane getRoot() {
+        return root;
+    }
 
     public MenuComponent(CrackGPT application)
     {
         this.application = application;
     }
 
-    public HBox generate() {
-        HBox menu = new HBox();
-        menu.getStyleClass().add("menu");
-        menu.setAlignment(Pos.CENTER);
-        menu.setPadding(new Insets(10));
 
-        Label newItem = new Label("New Chat");
-        Label historyItem = new Label("History");
-        menu.getChildren().addAll(newItem, historyItem);
+    private ListView<String> createChatHistoryListView(){
+        ListView<String> listView = new ListView<>();
+        listView.getItems().add("Chat 1");
 
-        return menu;
+
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->{
+            if(newValue != null){
+                for(Tab t : chatTabPane.getTabs()) {
+                    if(t.getText().equals(newValue)) {
+                        chatTabPane.getSelectionModel().select(t);
+                        break;
+                    }
+                }
+            }
+        });
+        listView.setPrefWidth(140);
+        return listView;
     }
+
+    private TabPane createChatTabPane(){
+        TabPane tabPane = new TabPane();
+        //tabPane.setVisible(false);
+        ChatScene body = new ChatScene(application);
+
+        Tab tab = new Tab("Chat 1");
+        tab.setContent(body.createBody());
+        tabPane.getTabs().add(tab);
+
+        tabPane.setTabMaxHeight(0);
+        tabPane.setTabMinHeight(0);
+
+        return tabPane;
+    }
+
+    private Button createNewChatButton(){
+
+        Button button = new Button("New Chat");
+        button.setOnAction(e -> {
+            //chatTabPane.setVisible(false);
+            chatCounter++;
+            String chatTitle = "Chat " + chatCounter;
+            chatHistoryListView.getItems().add(chatTitle);
+
+            ChatScene newBody = new ChatScene(application);
+
+            Tab newTab = new Tab(chatTitle);
+            newTab.setContent(newBody.createBody());
+            chatTabPane.getTabs().add(newTab);
+        });
+        return button;
+    }
+
+    public void menuInitialize(){
+        root= new BorderPane();
+
+        chatHistoryListView = createChatHistoryListView();
+        chatTabPane = createChatTabPane();
+        Button newChatButton = createNewChatButton();
+
+        HBox topMenu = new HBox();
+        topMenu.setPadding(new Insets(10));
+        //topMenu.setAlignment(Pos.CENTER);
+        topMenu.getChildren().add(newChatButton);
+/*
+        VBox leftMenu = new VBox(chatHistoryListView);
+        leftMenu.setPadding(new Insets(10));
+        leftMenu.setSpacing(10);
+        leftMenu.setPrefWidth(150);
+
+        VBox mainContent = new VBox();
+        mainContent.setSpacing(10);
+        mainContent.setPadding(new Insets(10));
+        VBox.setVgrow(chatTabPane, Priority.ALWAYS);
+
+        mainContent.getChildren().addAll(topMenu, chatTabPane);
+
+        HBox content = new HBox();
+        content.getChildren().addAll(leftMenu, mainContent);
+        root.setCenter(content);
+ */
+        root.setTop(topMenu);
+        root.setCenter(chatTabPane);
+        root.setLeft(chatHistoryListView);
+
+    }
+
+
 }
+
+
+//////
+
