cfd72a1841275c2a6b8a266336dae8e949af5b65
Timo Brouwer <timobrouwer@timos-mbp.wireless.hhs.nl>
2024-05-23 12:21:37 +0200

    Bug opgelost

--- src/main/java/org/example/crackgui/SettingsComponent.java
+++ src/main/java/org/example/crackgui/SettingsComponent.java
@@ -19,6 +19,7 @@ public class SettingsComponent {
 
     public SettingsComponent(CrackGPT application) {
         this.application = application;
+        this.language = "Dutch";
     }
 
     public String getLanguage() {
